07264a82fb2a70d45339bb777b0d0056c6dbd18e
Mark <Mark.wike@gmail.com>
2022-04-06 08:35:51 +0100

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Mark Wike on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
